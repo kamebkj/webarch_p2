bc7bf31b35df861610c42a8133cf5af3f8c52861
Kate <katehsiao@ischool.berkeley.edu>
2012-12-06 19:19:29 -0800

    Add responsive nav bar

--- server/templates/touch.js
+++ server/templates/touch.js
@@ -1,29 +1,47 @@
 $(document).ready(function(){
 
-	var obj = document.getElementById('g1');
+	var obj1 = document.getElementById('g1');
 	
-	obj.addEventListener('touchstart', function(event) {
+	obj1.addEventListener('touchstart', function(event) {
 		event.preventDefault();
                 if (event.targetTouches.length == 1) {
-			obj.style.backgroundColor = "#F9D654";
+			obj1.style.backgroundColor = "#F9D654";
 		}
 	}, false);
 
-	obj.addEventListener('touchend', function(event) {
+	obj1.addEventListener('touchend', function(event) {
 		event.preventDefault();
-		obj.style.backgroundColor = "#F9E5CC";		
+		obj1.style.backgroundColor = "#F9E5CC";		
         }, false);
-/*
-	obj.addEventListener('touchmove', function(event) {
-		event.preventDefault();
-		if (event.targetTouches.length == 1) {
-			//alert("bkj");
-			var touch = event.targetTouches[0];
-			obj.style.left = touch.pageX + 'px';
-			obj.style.top = touch.pageY + 'px';
-		}
-	}, false);
-*/
+
+        var obj2 = document.getElementById('g2');
+
+        obj2.addEventListener('touchstart', function(event) {
+                event.preventDefault();
+                if (event.targetTouches.length == 1) {
+                        obj2.style.backgroundColor = "#F9D654";
+                }
+        }, false);
+
+        obj2.addEventListener('touchend', function(event) {
+                event.preventDefault();
+                obj2.style.backgroundColor = "#F9E5CC";
+        }, false);
+
+        var obj3 = document.getElementById('g3');
+
+        obj3.addEventListener('touchstart', function(event) {
+                event.preventDefault();
+                if (event.targetTouches.length == 1) {
+                        obj3.style.backgroundColor = "#F9D654";
+                }
+        }, false);
+
+        obj3.addEventListener('touchend', function(event) {
+                event.preventDefault();
+                obj3.style.backgroundColor = "#F9E5CC";
+        }, false);
+
 
 });
 
